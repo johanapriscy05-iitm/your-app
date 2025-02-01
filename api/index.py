ecc2e25a6cab6058bf61b98aba0e7fffbd8ac445
johanapriscy05-iitm <23f2001738@ds.study.iitm.ac.in>
2025-02-01 16:24:53 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -22,7 +22,9 @@ class handler(BaseHTTPRequestHandler):
         # Enable CORS
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
-        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Access-Control-Allow-Methods", "GET, OPTIONS")
+        self.send_header("Access-Control-Allow-Headers", "Content-Type")
         self.end_headers()
 
         # Send response
