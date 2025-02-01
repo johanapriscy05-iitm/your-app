097ce67289abf1dc4f2ee239fe0bd1eaf12afb44
johanapriscy05-iitm <23f2001738@ds.study.iitm.ac.in>
2025-02-02 00:00:23 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,42 +3,7 @@ from http.server import BaseHTTPRequestHandler
 import urllib.parse
 
 # Define the student marks dictionary with updated data
-student_marks = [
-    {"name": "hPZTeg", "marks": 33}, {"name": "ucIF7L", "marks": 60}, {"name": "FHpnPdi", "marks": 38},
-    {"name": "Y5E6KJQsF", "marks": 66}, {"name": "B7Bfrp7", "marks": 13}, {"name": "0e4ovvtj", "marks": 82},
-    {"name": "4lTJW", "marks": 40}, {"name": "2vL", "marks": 42}, {"name": "NSVK", "marks": 57},
-    {"name": "OPl4v2", "marks": 27}, {"name": "dMPm89tU", "marks": 20}, {"name": "s6ecnv", "marks": 80},
-    {"name": "grBmXBny", "marks": 58}, {"name": "h5e", "marks": 17}, {"name": "V0g7Wp", "marks": 53},
-    {"name": "ccU3cF1OgL", "marks": 4}, {"name": "ipaXvBsci", "marks": 81}, {"name": "vmzexfH", "marks": 2},
-    {"name": "s", "marks": 75}, {"name": "dP", "marks": 25}, {"name": "QjX0C", "marks": 99},
-    {"name": "PIXXfxNKOA", "marks": 79}, {"name": "N9L1WSWhR", "marks": 60}, {"name": "JD0qIarc", "marks": 12},
-    {"name": "hYyx", "marks": 65}, {"name": "3e", "marks": 37}, {"name": "kF474Zj", "marks": 43},
-    {"name": "bccF", "marks": 38}, {"name": "UFIQfHW", "marks": 89}, {"name": "fHYbA", "marks": 44},
-    {"name": "hc5", "marks": 28}, {"name": "wHT", "marks": 41}, {"name": "K", "marks": 73},
-    {"name": "BH2XN", "marks": 6}, {"name": "dk", "marks": 24}, {"name": "NToh1MJ2", "marks": 22},
-    {"name": "AtYrrCz", "marks": 9}, {"name": "xcMeAwC", "marks": 62}, {"name": "2aib8m", "marks": 3},
-    {"name": "vbdr", "marks": 97}, {"name": "M", "marks": 26}, {"name": "8", "marks": 61},
-    {"name": "P4zH", "marks": 63}, {"name": "2ql3p", "marks": 34}, {"name": "Qmh", "marks": 85},
-    {"name": "s", "marks": 66}, {"name": "7O", "marks": 51}, {"name": "uINS", "marks": 56},
-    {"name": "wN5lTe8", "marks": 85}, {"name": "C88lI6tE", "marks": 24}, {"name": "Dzq", "marks": 50},
-    {"name": "jduS", "marks": 74}, {"name": "qvL2LEQ50c", "marks": 77}, {"name": "hKvnGqBau", "marks": 94},
-    {"name": "xdD", "marks": 33}, {"name": "tymi8p3", "marks": 82}, {"name": "DTdZ", "marks": 80},
-    {"name": "pjW701", "marks": 66}, {"name": "yUpHIIwK", "marks": 72}, {"name": "HLhZXowh", "marks": 89},
-    {"name": "lua9iBm7", "marks": 46}, {"name": "nlUBb", "marks": 36}, {"name": "nm", "marks": 32},
-    {"name": "Dcdg", "marks": 66}, {"name": "ufsoGMP", "marks": 36}, {"name": "D6MYc8K", "marks": 7},
-    {"name": "fHp7EgvW", "marks": 65}, {"name": "Qw", "marks": 66}, {"name": "Hvt", "marks": 58},
-    {"name": "WqB7zH", "marks": 29}, {"name": "KC", "marks": 93}, {"name": "LWruUv4YRP", "marks": 87},
-    {"name": "gya6H3ch", "marks": 98}, {"name": "n", "marks": 9}, {"name": "uPPSz", "marks": 11},
-    {"name": "G8gm8P", "marks": 18}, {"name": "FlMO", "marks": 1}, {"name": "4nq", "marks": 34},
-    {"name": "2Avdeoht", "marks": 96}, {"name": "epr", "marks": 53}, {"name": "7vbcE", "marks": 27},
-    {"name": "Ju", "marks": 78}, {"name": "Kn8oXS7c", "marks": 30}, {"name": "V3yVgga", "marks": 82},
-    {"name": "BdmsIG", "marks": 74}, {"name": "Tw", "marks": 73}, {"name": "6RG", "marks": 82},
-    {"name": "CMQyRg", "marks": 24}, {"name": "87", "marks": 17}, {"name": "sI8x7", "marks": 82},
-    {"name": "5PGM", "marks": 50}, {"name": "rX7aJACLfA", "marks": 80}, {"name": "FtUp4RA", "marks": 37},
-    {"name": "K90v8", "marks": 76}, {"name": "RAqoU", "marks": 0}, {"name": "ZQpiT", "marks": 18},
-    {"name": "aI", "marks": 70}, {"name": "SBQzXYmNl", "marks": 27}, {"name": "p", "marks": 56},
-    {"name": "5PfZp", "marks": 14}
-]
+student_marks = [{"name":"LYxs","marks":58},{"name":"KmAOZuB5h","marks":45},{"name":"2EHyD3m","marks":67},{"name":"65ekYL","marks":0},{"name":"ec8HQ","marks":30},{"name":"38ZRV","marks":95},{"name":"aeuA","marks":10},{"name":"DFCC9Z","marks":8},{"name":"H0N0","marks":4},{"name":"f0MahL","marks":12},{"name":"I5ZQ6","marks":73},{"name":"eK","marks":60},{"name":"xx","marks":98},{"name":"rrMOHGKdz","marks":51},{"name":"RdgWED1mu2","marks":85},{"name":"PjITQM8R","marks":4},{"name":"sdqPy","marks":38},{"name":"rI","marks":73},{"name":"nlwI1","marks":62},{"name":"HiMQ","marks":35},{"name":"uQ","marks":51},{"name":"Yv84e3hS","marks":0},{"name":"0Yn2Z","marks":55},{"name":"4dsBfpL","marks":36},{"name":"F","marks":71},{"name":"RYCUm","marks":87},{"name":"JX","marks":49},{"name":"drQ4g6WPWG","marks":51},{"name":"mEUc306","marks":14},{"name":"uU0C32Y","marks":60},{"name":"QQoUK","marks":54},{"name":"1T","marks":5},{"name":"hFYx55AuN","marks":8},{"name":"j9g","marks":3},{"name":"1wR","marks":97},{"name":"5ONC","marks":93},{"name":"5K","marks":19},{"name":"X","marks":40},{"name":"gNUD2LYrc","marks":37},{"name":"HoR","marks":17},{"name":"m3N","marks":74},{"name":"Kn","marks":87},{"name":"ykU0YJSlmv","marks":84},{"name":"Ow","marks":3},{"name":"Qy","marks":59},{"name":"lYYE9","marks":29},{"name":"czY","marks":18},{"name":"tr","marks":0},{"name":"Txct","marks":23},{"name":"A0K","marks":12},{"name":"d","marks":64},{"name":"RHLnO","marks":13},{"name":"uu","marks":54},{"name":"PlJIuQ6waS","marks":96},{"name":"h1AKC","marks":79},{"name":"RAUZysmA8","marks":80},{"name":"ZecL1LJVo","marks":26},{"name":"y","marks":47},{"name":"5DpUveB3","marks":31},{"name":"F0ENb","marks":23},{"name":"YaXL","marks":53},{"name":"r","marks":5},{"name":"YmcCoZX1","marks":0},{"name":"AZahlo","marks":13},{"name":"8CbOiEezO","marks":0},{"name":"06G8nTz","marks":11},{"name":"y2id4","marks":83},{"name":"8yoduIN0","marks":71},{"name":"CWt7IjPtEn","marks":87},{"name":"1kaBRZ7m","marks":77},{"name":"55R8AZ","marks":50},{"name":"i1zHg8R4d","marks":40},{"name":"jqkxa6nEc","marks":77},{"name":"4ddH","marks":17},{"name":"4IHPsXh","marks":84},{"name":"Q","marks":1},{"name":"KCTir","marks":80},{"name":"XPx1","marks":28},{"name":"7iNI","marks":5},{"name":"kh","marks":37},{"name":"W6qE3","marks":70},{"name":"R4","marks":68},{"name":"cE","marks":78},{"name":"RTPhlUx","marks":90},{"name":"9cAZTjI","marks":96},{"name":"ZrM7go","marks":44},{"name":"K8qURF3DD","marks":97},{"name":"zzSmnTt","marks":74},{"name":"tNl0r1QFJ8","marks":91},{"name":"kQ","marks":25},{"name":"zIySbnat5H","marks":34},{"name":"bl22","marks":17},{"name":"ItloxTu","marks":96},{"name":"dAxmsGz","marks":45},{"name":"IaB43","marks":15},{"name":"TJ","marks":21},{"name":"RLjko","marks":19},{"name":"5ranyB","marks":44},{"name":"mV","marks":40},{"name":"wd9XqxiGgQ","marks":11}]
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
